cf83eccb601464fa6f74a85213f3842018957e90
ryangholland <rghcode@gmail.com>
2023-02-21 19:43:53 -0500

    Add game function to play a 5-round game and declare winner

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ function playRound(playerChoice, computerChoice) {
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
       console.log(`Tie. You and computer both chose rock.`);
+      return "tie";
     } else if (computerChoice === "paper") {
       console.log(`Computer wins the round! Computer's paper beat your rock.`);
+      return "computerWin";
     } else if (computerChoice === "scissors") {
       console.log(`You win the round! Your rock beat computer's scissors.`);
+      return "playerWin";
     }
   }
 
   if (playerChoice === "paper") {
     if (computerChoice === "rock") {
       console.log(`You win the round! Your paper beat computer's rock.`);
+      return "playerWin";
     } else if (computerChoice === "paper") {
       console.log(`Tie. You and computer both chose paper.`);
+      return "tie";
     } else if (computerChoice === "scissors") {
       console.log(
         `Computer wins the round! Computer's scissors beat your paper.`
       );
+      return "computerWin";
     }
   }
 
@@ -31,14 +37,35 @@ function playRound(playerChoice, computerChoice) {
       console.log(
         `Computer wins the round! Computer's rock beat your scissors.`
       );
+      return "computerWin";
     } else if (computerChoice === "paper") {
       console.log(`You win the round! Your scissors beat computer's paper.`);
+      return "playerWin";
     } else if (computerChoice === "scissors") {
       console.log(`Tie. You and computer both chose scissors.`);
+      return "tie";
     }
   }
 }
 
-let playerChoice = prompt("Choose your weapon:").toLowerCase();
-let computerChoice = getComputerChoice();
-playRound(playerChoice, computerChoice);
+function game() {
+  let playerWins = 0;
+  let computerWins = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt("Choose your weapon:").toLowerCase();
+    let computerChoice = getComputerChoice();
+    let result = playRound(playerChoice, computerChoice);
+
+    if (result === "playerWin") playerWins++;
+    if (result === "computerWin") computerWins++;
+  }
+
+  console.log("\n");
+  console.log(`Final Score`);
+  console.log(`------------`);
+  console.log(`Player: ${playerWins}`);
+  console.log(`Computer: ${computerWins}`);
+}
+
+game();
