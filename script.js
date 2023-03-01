f6dbf9263e501684f06c0c4f850dfc101762ac84
ryangholland <rghcode@gmail.com>
2023-02-28 21:07:54 -0500

    Display round winner to the DOM instead of console

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const buttons = document.querySelectorAll("button");
+const results = document.getElementById("results");
+
 function getComputerChoice() {
   const CHOICES = ["rock", "paper", "scissors"];
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
@@ -6,43 +9,39 @@ function getComputerChoice() {
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
-      console.log(`Tie. You and computer both chose rock.`);
+      results.textContent = `Tie. You and computer both chose rock.`;
       return "tie";
     } else if (computerChoice === "paper") {
-      console.log(`Computer wins the round! Computer's paper beat your rock.`);
+      results.textContent = `Computer wins the round! Computer's paper beat your rock.`;
       return "computerWin";
     } else if (computerChoice === "scissors") {
-      console.log(`You win the round! Your rock beat computer's scissors.`);
+      results.textContent = `You win the round! Your rock beat computer's scissors.`;
       return "playerWin";
     }
   }
 
   if (playerChoice === "paper") {
     if (computerChoice === "rock") {
-      console.log(`You win the round! Your paper beat computer's rock.`);
+      results.textContent = `You win the round! Your paper beat computer's rock.`;
       return "playerWin";
     } else if (computerChoice === "paper") {
-      console.log(`Tie. You and computer both chose paper.`);
+      results.textContent = `Tie. You and computer both chose paper.`;
       return "tie";
     } else if (computerChoice === "scissors") {
-      console.log(
-        `Computer wins the round! Computer's scissors beat your paper.`
-      );
+      results.textContent = `Computer wins the round! Computer's scissors beat your paper.`;
       return "computerWin";
     }
   }
 
   if (playerChoice === "scissors") {
     if (computerChoice === "rock") {
-      console.log(
-        `Computer wins the round! Computer's rock beat your scissors.`
-      );
+      results.textContent = `Computer wins the round! Computer's rock beat your scissors.`;
       return "computerWin";
     } else if (computerChoice === "paper") {
-      console.log(`You win the round! Your scissors beat computer's paper.`);
+      results.textContent = `You win the round! Your scissors beat computer's paper.`;
       return "playerWin";
     } else if (computerChoice === "scissors") {
-      console.log(`Tie. You and computer both chose scissors.`);
+      results.textContent = `Tie. You and computer both chose scissors.`;
       return "tie";
     }
   }
@@ -72,8 +71,6 @@ function game() {
 game();
 */
 
-const buttons = document.querySelectorAll("button");
-
 for (let button of buttons) {
   button.addEventListener("click", (e) => {
     let playerChoice = e.target.id;
@@ -81,3 +78,5 @@ for (let button of buttons) {
     playRound(playerChoice, computerChoice);
   });
 }
+
+console.log(results);
